4ccc50b53f1e63a5977a087af2430a58a51a3895
Godisouleymane <ssabiougodi@gmail.com>
2023-07-01 06:05:42 +0100

    init

--- javascript/app.js
+++ javascript/app.js
@@ -1,6 +1,20 @@
-$(document).ready(function () {
-    $('#action_menu_btn').click(function () {
-        $('.action_menu').toggle();
-    });
-});
+var menuButton = document.getElementById('menuButton');
 
+var menuItems = document.getElementById('menuItems');
+
+
+// ajouter un gestionnaire d'evenement sur le button de menu
+
+menuButton.addEventListener('click', function () {
+    // verifier si la liste des elements de menu est visible
+
+    if (menuItems.style.display === 'none') {
+        // aficher la liste des elements de menu
+
+        menuItems.style.display = 'block';
+    } else{
+        // masquez la liste des elements de menu 
+
+        menuItems.style.display = 'none';
+    }
+})
